8121bc5a335e6a84ed99f2e3d17ce1c4286f76f0
arashtari <arashtari@gmail.com>
2023-05-10 21:53:20 -0400

    midification de pied.php

--- vino_etu/vues/pied.php
+++ vino_etu/vues/pied.php
@@ -1,13 +1,25 @@
 
-		</main>
+		
 		<footer>
-			<p>
-				<a href="http://www.cmaisonneuve.qc.ca/"></a>
-			</p>
-			<p>
-				Réalisé par <a href="mailto:jmartel@cmaisonneuve.qc.ca">Jonathan Martel</a>. Sous licence Creative Commons BY-NC 4.0 (2014)
-			</p>
-		</footer>
-	</body>
-
+      <div class="footer">
+        <div class="footer-titre">
+          <p>PARTAGEZ-NOUS</p>
+        </div>
+        <div class="footer-font-awesome">
+          <i class="fa fa-facebook-square"></i>
+          <i class="fa fa-linkedin-square"></i>
+          <i class="fa fa-twitter-square"></i>
+          <i class="fa fa-youtube-play"></i>
+        </div>
+    </div>
+    <div class="bas-footer">
+      <div><i class="fa fa-copyright"></i>projet web2 - 2023</div>
+      <div>Mentions légales - Politique de confidentialité</div>
+    </div>
+	<p><a href="http://www.cmaisonneuve.qc.ca/"></a></p>
+	  <p>Réalisé par <a href="mailto:jmartel@cmaisonneuve.qc.ca">Jonathan Martel</a>. Sous licence Creative Commons BY-NC 4.0 (2014)</p>
+    </footer>
+ 
 </html>
+
+		
