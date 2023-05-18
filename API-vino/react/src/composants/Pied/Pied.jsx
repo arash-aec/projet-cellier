e2e004e1f13e909ba25157737f23cd975c737770
SamNzri <Nazariii.saman@gmail.com>
2023-05-18 11:31:03 -0400

    ajoute dossier images dans public et modifie page acceuil et pied

---
+++ API-vino/react/src/composants/Pied/Pied.jsx
@@ -0,0 +1,56 @@
+import './Pied.css'
+
+const Footer = () => {
+  
+  return (
+  
+       
+    
+    <div className="footer">
+      <section className="footer-partage">
+        <h2>PARTAGEZ-NOUS</h2>
+        <div className="footer-partage-icone">
+          <i className="fa fa-facebook-square"></i>
+          <i className="fa fa-linkedin-square"></i>
+          <i className="fa fa-twitter-square"></i>
+          <i className="fa fa-youtube-play"></i>
+        </div>
+      </section>
+
+      <section className="footer-copyright">
+        <p><i className="fa fa-copyright"></i> Projet Web 2 - 2023</p>
+        <p className="footer-copyright-nom">Vino</p>
+        <p>Mentions légales - Politique de confidentialité</p>
+      </section>
+
+  
+    </div>
+
+    
+  )
+}
+export default Footer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+
+      
