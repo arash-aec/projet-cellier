58df3c53ad6ceea49891fb27a5e2abe22e4c5cb1
jeromeFosse <jeromefosse66@gmail.com>
2023-05-25 21:13:27 -0400

    petite modification dans react

--- API-vino/react/src/Router.jsx
+++ API-vino/react/src/Router.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import React from "react";
 import Footer from "./composants/Pied/Pied";
 import Entete from "./composants/Entete/Entete";
-import Usager from './vues/Usager/Usager';
 import NonTrouve from "./vues/NonTrouve/NonTrouve";
 import Accueil from "./vues/Accueil/Accueil";
 import ListeBouteille from "./vues/ListeBouteille/ListeBouteille";
@@ -16,7 +15,6 @@ const AppRouter = () => {
       <Entete />
       <Routes>
         <Route path="/" element={<Accueil />} />
-        <Route path="/usager" element={<Usager />} />
         <Route path="/listeBouteille/:id" element={<ListeBouteille />} />
         <Route path="/celliers" element={<ListeCellier />} />
         <Route path="*" element={<NonTrouve />} />
--- API-vino/react/src/composants/Entete/Entete.jsx
+++ API-vino/react/src/composants/Entete/Entete.jsx
@@ -7,7 +7,7 @@ const Entete = () => {
 
     <div>
         <header className="header">
-          <Link to="/" className="logo"><img src="images/logo-1.png" alt="logo de Vino"/></Link>
+          <Link to="/" className="logo"><img src="../public/images/logo-1.png" alt="logo de Vino"/></Link>
           <input className="side-menu" type="checkbox" id="side-menu" />
           <label className="hamb" htmlFor="side-menu">
               <span className="hamb-line"></span>
@@ -15,7 +15,7 @@ const Entete = () => {
           <nav className="nav">
             <ul className="menu">
             <li><a href="/">Accueil</a></li>
-            <li><a href="/celliers">Cellier</a></li>
+            <li><a href="/celliers">Mes Celliers</a></li>
             </ul>
           </nav>
         </header>
